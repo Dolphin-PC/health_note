cd6cefdaf94eb8e8bc064fd1a9e0158b4ecaad29
jake <pcx474@gmail.com>
2023-02-27 18:34:16 +0900

    home screen 완료

---
+++ lib/screens/statics_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StaticsScreen extends StatefulWidget {
+  const StaticsScreen({Key? key}) : super(key: key);
+
+  @override
+  State<StaticsScreen> createState() => _StaticsScreenState();
+}
+
+class _StaticsScreenState extends State<StaticsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
